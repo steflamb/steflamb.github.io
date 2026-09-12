0be1436a2bd6d929026fbe22d6c43019000b2974
Stefano Carlo Lambertenghi <stefanolambe@me.com>
2026-09-12 19:18:47 +0200

    fix

--- src/data/site.ts
+++ src/data/site.ts
@@ -5,7 +5,7 @@ export const navItems = [
   { label: 'Publications', href: '/publications/' },
   { label: 'About', href: '/about/' },
   { label: 'CV', href: '/cv/' },
-  { label: 'Garage', href: '/garage/' },
+  { label: 'Just for fun', href: '/garage/' },
 ];
 
 export const profileLinks = {
